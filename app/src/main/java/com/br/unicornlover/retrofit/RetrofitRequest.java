090d0f81a7bbf757c6f9e3facfc8f6614fe7154a
Stanislao <stanis.campregher@gmail.com>
2023-05-19 23:17:32 -0300

    add Logging OkHttp para debug

--- app/src/main/java/com/br/unicornlover/retrofit/RetrofitRequest.java
+++ app/src/main/java/com/br/unicornlover/retrofit/RetrofitRequest.java
@@ -1,7 +1,11 @@
 package com.br.unicornlover.retrofit;
 
+import androidx.annotation.NonNull;
+
 import com.br.unicornlover.BuildConfig;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,9 +13,19 @@ public class RetrofitRequest {
 
     private static Retrofit retrofit;
 
+    @NonNull
+    public static OkHttpClient client() {
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        return new OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+    }
+
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
+                    .client(client())
                     .baseUrl(BuildConfig.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
